1040c96df89ca5deccaff9e6e06e9a015129d468
TejasPrasad <tejasprasad43@gmail.com>
2024-03-27 15:18:07 +0530

    my first flutter project

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage({super.key});
+  
+  @override
+  State<CurrencyConverterMaterialPage> createState() => _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromARGB(255, 0, 0, 0),
+        width: 1,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(0, 169, 184, 1),
+        elevation: 0,
+        title: const Text(
+          "USD to INR Converter",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: const Color.fromRGBO(0, 169, 184, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '₹ $result',
+              style: const TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: textEditingController,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontFamily: 'Roboto',
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'Enter the amount in INR',
+                  hintStyle: TextStyle(color: Colors.white60),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.white,
+                  filled: true,
+                  fillColor: Colors.white60,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 83.35;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 15,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text('Convert'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
